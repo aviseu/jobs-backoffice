f01cc9ca2843a36df636b6e2ee5f481ff58b6887
aviseu <alexandre@viseu.me>
2025-02-10 00:01:37 +0100

    initial commit with import binary

---
+++ cmd/import/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/kelseyhightower/envconfig"
+	"log/slog"
+	"os"
+)
+
+type config struct {
+	Log struct {
+		Level slog.Level `default:"info"`
+	}
+}
+
+func main() {
+	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{})))
+
+	if err := run(context.Background()); err != nil {
+		slog.Error(err.Error())
+		os.Exit(1)
+	}
+}
+
+func run(ctx context.Context) error {
+	// load environment variables
+	slog.Info("loading environment variables...")
+	var cfg config
+	if err := envconfig.Process("", &cfg); err != nil {
+		return fmt.Errorf("failed to load environment variables: %w", err)
+	}
+
+	// configure logging
+	slog.Info("configuring logging...")
+	log := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: cfg.Log.Level}))
+	slog.SetDefault(log)
+
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/aviseu/jobs
+
+go 1.23.1
+
+require github.com/kelseyhightower/envconfig v1.4.0 // indirect
