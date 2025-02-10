af5d87181acfd87abcd26f6846b42600d6edc9ff
aviseu <alexandre@viseu.me>
2025-02-10 23:19:57 +0100

    main for the api + runConfigurations for local

---
+++ cmd/api/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aviseu/jobs/internal/app/storage"
+	"github.com/jmoiron/sqlx"
+	"log/slog"
+	"os"
+
+	"github.com/kelseyhightower/envconfig"
+	_ "github.com/lib/pq"
+)
+
+type config struct {
+	DB  storage.Config
+	Log struct {
+		Level slog.Level `default:"info"`
+	}
+}
+
+func main() {
+	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{})))
+
+	if err := run(); err != nil {
+		slog.Error(err.Error())
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	// load environment variables
+	slog.Info("loading environment variables...")
+	var cfg config
+	if err := envconfig.Process("", &cfg); err != nil {
+		return fmt.Errorf("failed to load environment variables: %w", err)
+	}
+
+	// configure logging
+	slog.Info("configuring logging...")
+	log := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: cfg.Log.Level}))
+	slog.SetDefault(log)
+
+	// setup database
+	slog.Info("setting up database...")
+	db, err := storage.SetupDatabase(cfg.DB)
+	if err != nil {
+		return fmt.Errorf("failed to setup database: %w", err)
+	}
+	defer func(db *sqlx.DB) {
+		err := db.Close()
+		if err != nil {
+			slog.Error(fmt.Errorf("failed to close database connection: %w", err).Error())
+		}
+	}(db)
+
+	return nil
+}
