2c6c036dab284cf75a7faeb7aef6d9c741a9afc9
aviseu <alexandre@viseu.me>
2025-02-11 02:47:36 +0100

    fixing typo on existing migration

--- config/migrations/000002_create_channels_table.up.sql
+++ config/migrations/000002_create_channels_table.up.sql
@@ -1,7 +1,7 @@
 create table channels (
     id uuid primary key,
     name text not null,
-    integration text not null,
+    integration int not null,
     status int not null,
     created_at timestamptz not null default now(),
     updated_at timestamptz not null default now()
