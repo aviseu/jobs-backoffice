882ee3ea2d5a2fd0a16fa1791ca08e862f206652
aviseu <alexandre@viseu.me>
2025-04-15 00:45:43 +0200

    removing unused request builders

--- internal/app/application/http/api/requests.go
+++ internal/app/application/http/api/requests.go
@@ -5,19 +5,6 @@ type CreateChannelRequest struct {
 	Integration string `json:"integration"`
 }
 
-func NewCreateChannelRequest(name, integration string) *CreateChannelRequest {
-	return &CreateChannelRequest{
-		Name:        name,
-		Integration: integration,
-	}
-}
-
 type UpdateChannelRequest struct {
 	Name string `json:"name"`
-}
-
-func NewUpdateChannelRequest(name string) *UpdateChannelRequest {
-	return &UpdateChannelRequest{
-		Name: name,
-	}
 }
