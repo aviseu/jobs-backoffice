3519330ea4595db74dbdf131b7d61d72cb690d92
aviseu <alexandre@viseu.me>
2025-04-18 14:39:51 +0200

    adding indexes to import_metrics

---
+++ config/migrations/000012_add_indexes_to_import_metrics.down.sql
@@ -0,0 +1,2 @@
+DROP INDEX IF EXISTS idx_import_metrics_import_id;
+DROP INDEX IF EXISTS idx_import_metrics_job_id;
---
+++ config/migrations/000012_add_indexes_to_import_metrics.up.sql
@@ -0,0 +1,2 @@
+CREATE INDEX IF NOT EXISTS idx_import_metrics_import_id ON import_metrics(import_id);
+CREATE INDEX IF NOT EXISTS idx_import_metrics_job_id ON import_metrics(job_id);
